cbc6b8c4386eb1106562fed1870bd4dc34eb7c00
Adel <adi.stein@outlook.cz>
2023-09-28 17:19:56 +0200

    Update Program.cs

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -4,82 +4,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApp4
 {
     internal class Knihovna
     {
-        static void Main(string[] args)
+        class Knihy_prvni
         {
+            public string Jmeno1 { get; set; }
+            public string Autor1 { get; set; }
+            public int Rok1 { get; set; }
 
-            String[] Knihy = new String[4];
+            public string Jmeno2 { get; set; }
+            public string Autor2 { get; set; }
+            public int Rok2 { get; set; }
 
-            Knihy[0] = "Obraz Doriana Graye";
-            Knihy[1] = "Obraz Doriana Graye";
-            Knihy[2] = "Romeo a Julie";
-            Knihy[3] = "Lakomec";
+            public string Jmeno3 { get; set; }
+            public string Autor3 { get; set; }
+            public int Rok3 { get; set; }
+
+            public string Jmeno4 { get; set; }
+            public string Autor4 { get; set; }
+            public int Rok4 { get; set; }
+        }
 
 
-            for (int i = 0; i < Knihy.Length; i++)
+        class Program
+        {
+            static void Main(string[] args)
             {
-                Console.WriteLine(Knihy[i]);
+                List<Knihy_prvni> prvni = new List<Knihy_prvni>();
+
+                prvni.Add(new Knihy_prvni { Jmeno1 = "Knihovna a hra", Autor1 = "Váš Jméno", Rok1 = 2023 });
+                prvni.Add(new Knihy_prvni { Jmeno2 = "Další kniha", Autor2 = "Autor XYZ", Rok2 = 2021 });
+
+
+
+                Console.WriteLine("Vítejte v knihovně přečtené literatury!");
+                bool running = true;
+
+                while (running)
+                {
+                    Console.WriteLine("1. Seznam knih");
+                    Console.WriteLine("2. Konec");
+
+                    string choice = Console.ReadLine();
+
+                    switch (choice)
+                    {
+                        case "1":
+                            Console.WriteLine("Z jakého ročníku knihu chceš?:");
+                            string odpoved = Console.ReadLine();
+                            switch(odpoved)
+                            {
+                                case "1":
+                                    foreach (var book in prvni)
+                                    {
+                                        Console.WriteLine($"{book.Jmeno1} - {book.Autor1} ({book.Rok1})");
+                                    }
+                                    break;
+
+                                case "2":
+                                    foreach (var book2 in prvni)
+                                    {
+                                        Console.WriteLine($"{book2.Jmeno2} - {book2.Autor2} ({book2.Rok2})");
+                                    }
+                                    break;
+                                case "3":
+                                    foreach (var book3 in prvni)
+                                    {
+                                        Console.WriteLine($"{book3.Jmeno3} - {book3.Autor3} ({book3.Rok3})");
+                                    }
+                                    break;
+
+                                case "4":
+                                    foreach (var book4 in prvni)
+                                    {
+                                        Console.WriteLine($"{book4.Jmeno4} - {book4.Autor4} ({book4.Rok4})");
+                                    }
+                                    break;
+                                default:
+                                    Console.WriteLine("Neplatná volba. Zkuste to znovu.");
+                                    break;
+
+                            }
+                            break;
+
+                        case "2":
+                            running = false;
+                            break;
+                        default:
+                            Console.WriteLine("Neplatná volba. Zkuste to znovu.");
+                            break;
+                    }
+                }
             }
-
-            Console.ReadKey();
-
-        }
-
-        static void Knihy1()
-        {
-            
-            String[] Knihy1 = new String[4];
-
-            Knihy1[0] = "Harry Poter";
-            Knihy1[1] = "Obraz Doriana Graye";
-            Knihy1[2] = "Romeo a Julie";
-            Knihy1[3] = "Lakomec";
-
-
-            for (int i = 0; i < Knihy1.Length; i++)
-            {
-                Console.WriteLine(Knihy1[i]);
-            }
-
-            Console.ReadKey();
-
-        }
-        static void Knihy2()
-        {
-
-            String[] Knihy2 = new String[4];
-
-            Knihy2[0] = "Romeo Julie a tma";
-            Knihy2[1] = "Mariša";
-            Knihy2[2] = "";
-
-        }
-
-        static void Knihy3()
-        {
-
-            String[] Knihy3 = new String[4];
-
-            Knihy3[0] = "Romeo Julie a tma";
-            Knihy3[1] = "Mariša";
-            Knihy3[2] = "";
-
-        }
-
-        static void Knihy4()
-        {
-
-            String[] Knihy4 = new String[4];
-
-            Knihy4[0] = "Romeo Julie a tma";
-            Knihy4[1] = "Mariša";
-            Knihy4[2] = "";
-
-
-
         }
     }
+
 }
