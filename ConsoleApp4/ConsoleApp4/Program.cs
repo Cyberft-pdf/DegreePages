ada34131dd9f186629bf47df6bfd890cd98fbf42
Adel <adi.stein@outlook.cz>
2023-10-05 21:55:44 +0200

    Update Program.cs
    adding animation

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 
@@ -14,18 +15,25 @@ namespace ConsoleApp4
             public string Jmeno1 { get; set; }
             public string Autor1 { get; set; }
             public int Rok1 { get; set; }
+            public string status1 { get; set; }
+
 
             public string Jmeno2 { get; set; }
             public string Autor2 { get; set; }
             public int Rok2 { get; set; }
+            public string status2 { get; set; }
+
 
             public string Jmeno3 { get; set; }
             public string Autor3 { get; set; }
             public int Rok3 { get; set; }
+            public string status3 { get; set; }
+
 
             public string Jmeno4 { get; set; }
             public string Autor4 { get; set; }
             public int Rok4 { get; set; }
+            public string status4 { get; set; }
         }
 
 
@@ -35,17 +43,35 @@ namespace ConsoleApp4
             {
                 List<Knihy_prvni> prvni = new List<Knihy_prvni>();
 
-                prvni.Add(new Knihy_prvni { Jmeno1 = "Knihovna a hra", Autor1 = "Váš Jméno", Rok1 = 2023 });
-                prvni.Add(new Knihy_prvni { Jmeno2 = "Další kniha", Autor2 = "Autor XYZ", Rok2 = 2021 });
+                prvni.Add(new Knihy_prvni { Jmeno1 = "Dorian Gray", Autor1 = "Oscar Wilde", Rok1 = 1890 });
+                prvni.Add(new Knihy_prvni { Jmeno1 = "Lakomec", Autor1 = "Moliér", Rok1 = 1668 });
+                prvni.Add(new Knihy_prvni { Jmeno1 = "Maryša", Autor1 = "Alois Mrštík a Vilém Mrštík", Rok1 = 1894 });
 
 
+                string text = "Vítejte v knihovně přečtené/naplánované literatury!\n\n\n\n";
 
-                Console.WriteLine("Vítejte v knihovně přečtené literatury!");
+                for (int i = 0; i <= text.Length; i++)
+                {
+                    Console.Clear();
+                    Console.SetCursorPosition(Console.WindowWidth / 2 - i / 2, Console.WindowHeight / 5);
+                    Console.Write(text.Substring(0, i));
+                    Thread.Sleep(100); 
+                }
+
+                string line = new string('/', Console.WindowWidth);
+                Console.WriteLine(line);
+                Console.WriteLine(" ");
+
                 bool running = true;
 
                 while (running)
                 {
+
+                    Thread.Sleep(1000);
+                    Console.WriteLine(" ");
+                    Console.SetCursorPosition(49, 15);
                     Console.WriteLine("1. Seznam knih");
+                    Console.SetCursorPosition(49, 17);
                     Console.WriteLine("2. Konec");
 
                     string choice = Console.ReadLine();
@@ -53,7 +79,9 @@ namespace ConsoleApp4
                     switch (choice)
                     {
                         case "1":
-                            Console.WriteLine("Z jakého ročníku knihu chceš?:");
+                            Console.Clear();
+                            Console.WriteLine(" ");
+                            Console.Write("Z jakého ročníku knihu chceš?: ");
                             string odpoved = Console.ReadLine();
                             switch(odpoved)
                             {
@@ -98,6 +126,8 @@ namespace ConsoleApp4
                             break;
                     }
                 }
+
+
             }
         }
     }
